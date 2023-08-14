bbafcb0976eb0b7ecabbcc2aca16613215c6630d
Stavrin <steffanleprince@gmail.com>
2023-08-14 22:00:08 +0100

    Changed to the correct version of Unity.

---
+++ Assets/ZubrCodeTest/Scripts/Sorting.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+public class Sorting : MonoBehaviour
+{
+    public string maddison = "Maddison Albert";
+    public string hester = "Hester Blankenship";
+    public string saara = "Saara Browne";
+    public string libbie = "Libbie Case";
+    public string dominick = "Dominick Howells";
+    public string abida = "Abida Hayden";
+    public string giorgia = "Giorgia Cotton";
+    public string rita = "Rita Smyth";
+    public string rania = "Rania Wolf";
+    public string zainab = "Zainab Sheldon";
+
+
+    void LastNameDescending()
+    {
+        
+    }
+    void FirstNameDescending()
+    {
+
+    }
+    void LastNameAscending()
+    {
+
+    }
+    void FirstNameAscending()
+    {
+
+    }
+
+}
