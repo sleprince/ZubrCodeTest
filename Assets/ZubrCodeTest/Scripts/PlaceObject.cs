458029d5ac1d3434ba6eaa3ec77aa5a3d097f415
Stavrin <steffanleprince@gmail.com>
2023-08-18 11:57:35 +0100

    Improved code comments.

--- Assets/ZubrCodeTest/Scripts/PlaceObject.cs
+++ Assets/ZubrCodeTest/Scripts/PlaceObject.cs
@@ -1,31 +1,31 @@
-using System.Collections.Generic;
+using System.Collections.Generic; //needed to use lists
 using UnityEngine;
-using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARFoundation; //this and below needed for AR functionality
 using UnityEngine.XR.ARSubsystems;
-using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
+using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch; //using for touchscreen functionality
 //the above as a variable so we don't have to reference the long namespace each time
 
 
 [RequireComponent(requiredComponent: typeof(ARRaycastManager),
     requiredComponent2: typeof(ARPlaneManager))]
 //the above so that this script can only be attached to gameobject with those
-//components, and those components can't be removed
+//components, and those components can't be removed, essential components for this to work
 
 
 public class PlaceObject : MonoBehaviour
 {
 
-    [SerializeField] private GameObject[] objectPrefab;
+    [SerializeField] private GameObject objectPrefab; //will attach cube here in editor
 
     private ARRaycastManager rayManager;
-    private ARPlaneManager planeManager;
+    //private ARPlaneManager planeManager;
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
-    //above initialise a new list to store all the raycast hits
+    //above, initialise a new list to store all the raycast hits
 
     private void Awake()
     {
         rayManager = GetComponent<ARRaycastManager>();
-        planeManager = GetComponent<ARPlaneManager>();
+        //planeManager = GetComponent<ARPlaneManager>();
     }
 
     private void OnEnable()
@@ -46,7 +46,7 @@ public class PlaceObject : MonoBehaviour
 
     }
 
-    private void FingerDown(EnhancedTouch.Finger finger)
+    private void FingerDown(EnhancedTouch.Finger finger) //info to do with the finger press passed in
     {
         if (finger.index != 0) //0 is the first in the index, so if using more than 1 finger
             //then don't do anything
@@ -61,14 +61,22 @@ public class PlaceObject : MonoBehaviour
 
     private void HandleRaycast(EnhancedTouch.Finger finger)
     {
+        //similar to raycast functionity but optimised for AR, raycast takes in screen postion of where pressed,
+        //adds list of hits to list initialised earlier, what we want to detect against (in this case plane
+        //within polygon, as the plane object = polygon)
+        //the other TrackableType possibilities: https://docs.unity3d.com/2018.3/Documentation/ScriptReference/Experimental.XR.TrackableType.html
+
         if (rayManager.Raycast(screenPoint: finger.currentTouch.screenPosition,
             hitResults: hits, trackableTypes: TrackableType.PlaneWithinPolygon))
         {
             foreach (ARRaycastHit hit in hits)
             {
-                int i = Random.Range(0, objectPrefab.Length);
+                //for each hit we want to create a pose object from the hit.pose data which determines
+                //position and orientation of whatever we hit on the plane
                 Pose pose = hit.pose;
-                GameObject obj = Instantiate(original: objectPrefab[i], position:
+
+                //instantiate a new cube from prefab at the location and rotation of object for correct normals
+                GameObject obj = Instantiate(original: objectPrefab, position:
                     pose.position, rotation: pose.rotation);
             }
 
